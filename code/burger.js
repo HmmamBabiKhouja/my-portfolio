c18aeac2324f791cff712fa252827d2f65a46ee0
Hmmambk1994 <bhmmam@gmail.com>
2019-10-23 01:02:23 +0200

    changing classes names

--- code/burger.js
+++ code/burger.js
@@ -2,12 +2,12 @@ let burger = document.querySelector("#burger");
 let l1=document.querySelector(".line-one");
 let l2=document.querySelector(".line-two");
 let l3=document.querySelector(".line-three");
-let burgerList=document.querySelector(".nav-links");
+let burgerList=document.querySelector(".nav-list");
 
 burger.addEventListener("click",()=>{
 
     l1.classList.toggle("line-one-acitve");
     l2.classList.toggle("line-two-acitve");
     l3.classList.toggle("line-three-acitve");
-    burgerList.classList.toggle("nav-links-active");
+    burgerList.classList.toggle("nav-list-active");
 });
